e705e88dfe87b036e714b84f8a71b0db1a321b32
Alex Vuong <vavuong@deakin.edu.au>
2021-06-16 00:33:43 +1000

    One test of yesno is false since we mock the module

--- yesno/src/App.test.js
+++ yesno/src/App.test.js
@@ -6,7 +6,7 @@ import ask from './Api';
 
 configure({ adapter: new Adapter() });
 
-jest.mock('./Api');
+jest.mock('./Api'); //  Mock the ask of Api to __mocks__/Api
 
 // test('renders learn react link', () => {
 //   render(<App />);
@@ -33,9 +33,10 @@ it('should display Internet Gods answer in both text and picture format', () => 
   const wrapper = shallow(<App />);
   wrapper.find('button').simulate('click');
 
+  //  Use return here, so that our test wait for the end of the returned Promise
   return Promise.resolve()
     .then(() => {
-      wrapper.update();
+      wrapper.update(); //  Re-render
 
       const answer = wrapper.find('.answer');
       expect(answer.find('h1').text()).toBe('no');
@@ -44,14 +45,20 @@ it('should display Internet Gods answer in both text and picture format', () => 
 });
 
 describe('API Caller', () => {
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should call YesNo API', () => {
     const fetchSpy = jest.spyOn(global, 'fetch')
       .mockImplementation(() => Promise.resolve({
         json: () => { },
       }));
-    //  Instead of making api call to external system
-    //  We use mockImplementation
+    //  Instead of making api call to external system, we use mockImplementation
     //  It replaces behaviour of fetch with our customed implementation (for testing)
+    // **
+    //  Our implementation returns a Promise since below, our true function ask() will returns a Promise
 
     return ask()
       .then(() => {
